6a6817aeaf6fee678716f99f0f95468f4541d0d7
Deepak Singh <deepakanhviet@gmail.com>
2020-12-27 18:11:28 -0500

    init commit

---
+++ controller/appController.js
@@ -0,0 +1,52 @@
+const Image = require("../models/Image");
+const getImages = async (req, res) => {
+  try {
+    let images = await Image.find({}, " -__v");
+    return res.status(200).json({ images, msg: "image info fetched"    });
+  } 
+  catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "some error occured" });
+  }
+};
+
+const uploadImage = async (req, res) => {
+  try {
+    if (req.file && req.file.path) {
+      const image = new Image({
+        description: req.body.desc,
+        url: req.file.path,
+      });
+      await image.save();
+      return res.status(200).json({ msg: "image successfully saved" });
+    } 
+    else {
+      console.log(req.file);
+      return res.status(422).json({ error: "invalid" });
+    }
+  } 
+  catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "some error occured" });
+  }
+};
+
+const deleteImage = async (req, res) => {
+  try {
+    Image.findByIdAndDelete(req.id, (err, docs) => {
+      if(err){
+        console.log(err)
+        return res.status(422).json({error: "Invalid ID"})
+      }
+    })
+  }
+  catch (error){
+    console.error(error);
+    return res.status(500).json({ error: "some error occured" }); 
+  }
+}
+
+module.exports = {
+  getImages,
+  uploadImage,
+};
