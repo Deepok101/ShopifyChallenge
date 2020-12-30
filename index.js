a3f7da40ed7ea19abaec4a1e4f16de86904d89c5
Deepak Singh <deepakanhviet@gmail.com>
2020-12-29 23:24:28 -0500

    fix attempt for build

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ if (process.env.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname, 'frontend/build')));
 }
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'), function(err) {
+        if (err) {
+            res.status(500).send(err)
+        }     
+    })  
+})
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
